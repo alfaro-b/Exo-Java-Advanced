48c729ef4b5a110d0b4068a85a94c42f4b5fa558
Bittori Alfaro <bittori.alfaro@fms-ea.com>
2026-09-22 14:40:30 +0200

    Ajout de l'exercice 3.3 Horloge avec thread

---
+++ src/fr/fms/thread/TestThreadTime.java
@@ -0,0 +1,57 @@
+package fr.fms.thread;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Affiche l'heure en temps réel à l'aide d'un thread.
+ */
+public class TestThreadTime {
+
+	public static void main(String[] args) {
+
+		// Création d'un thread avec un délai d'une seconde
+		Thread thread = new Thread(new MonRunnable(1000));
+
+		thread.start();
+	}
+
+	/**
+	 * Tâche chargée d'afficher l'heure à intervalles réguliers.
+	 */
+	private static class MonRunnable implements Runnable {
+
+		private long delai;
+
+		/**
+		 * Crée la tâche avec le délai indiqué.
+		 *
+		 * @param delai délai entre deux affichages en millisecondes
+		 */
+		public MonRunnable(long delai) {
+			this.delai = delai;
+		}
+
+		@Override
+		public void run() {
+			DateFormat df = new SimpleDateFormat("HH:mm:ss");
+
+			while (true) {
+				
+				// Affiche l'heure courante
+				// System.out.println(df.format(new Date()));
+				// \r ramène le curseur au début de la ligne
+				System.out.print("\r" + df.format(new Date()));
+				
+				try {
+					// Attend une seconde avant le prochain affichage
+					Thread.sleep(delai);
+
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
